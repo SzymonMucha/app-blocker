54f69ee92e5c736d02a7129fe9bb90ab855eed39
SzymonMucha <86188861+SzymonMucha@users.noreply.github.com>
2026-08-09 13:34:28 +0200

    verification of package usage stats permission

--- app/src/main/java/com/example/app_blocker/MainActivity.java
+++ app/src/main/java/com/example/app_blocker/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.app_blocker;
 
+import android.app.AppOpsManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private boolean isPackageUsageStatsPermGranted(){
+        AppOpsManager appOps = (AppOpsManager) this.getSystemService(Context.APP_OPS_SERVICE);
+        int mode = appOps.checkOpNoThrow("android:get_usage_stats",
+                android.os.Process.myUid(), this.getPackageName());
+
+        return mode == AppOpsManager.MODE_ALLOWED;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //checks if package usage stats permission is granted
+        if(!isPackageUsageStatsPermGranted()) {
+            startActivity(new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS));
+        }
     }
 }
